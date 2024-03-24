d9096929e783d2bfb1972b675ffdc95a6879ae43
NatanSiilva <natanelsilva.jhs@gmail.com>
2024-03-23 23:01:33 -0300

    refactor: changing the balances microservice gateway to 3003

--- balances/src/adapters/web/server.py
+++ balances/src/adapters/web/server.py
@@ -47,4 +47,4 @@ app.include_router(root_router)
 app.include_router(balances_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=3003)
